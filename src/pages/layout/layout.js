f39b17ca639352c54e75d191afdbeebb217e4b99
Steven Chapman <stevey.cee@gmail.com>
2022-02-21 16:40:33 +0000

    styling: update layout

--- src/pages/layout/layout.js
+++ src/pages/layout/layout.js
@@ -12,23 +12,25 @@ import ReactEventObject from "../../pages/react-event-object/react-event-object"
 
 function Layout() {
   return (
-    <div className="container">
+    <>
       <Header />
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Layout />}></Route>
-          <Route index element={<Home />} />
-          <Route
-            path="passing-arguements"
-            element={<PassingArgumentsEventHandler />}
-          />
-          <Route path="event-handlers" element={<EventHandlers />} />
-          <Route path="props" element={<Props />} />
-          <Route path="react-event-object" element={<ReactEventObject />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+      <div className="container">
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Layout />}></Route>
+            <Route index element={<Home />} />
+            <Route
+              path="passing-arguements"
+              element={<PassingArgumentsEventHandler />}
+            />
+            <Route path="event-handlers" element={<EventHandlers />} />
+            <Route path="props" element={<Props />} />
+            <Route path="react-event-object" element={<ReactEventObject />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
