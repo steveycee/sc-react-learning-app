a0af4aa642e2da0e1f5063419e39a8a3f3eac310
Steven Chapman <stevey.cee@gmail.com>
2022-02-22 15:14:53 +0000

    feat: code block how to precursor

--- src/pages/how-to/code-blocks/code-blocks-how-to.js
+++ src/pages/how-to/code-blocks/code-blocks-how-to.js
@@ -4,7 +4,8 @@ function HowToCodeBlocks() {
   return (
     <>
       <h2>How To: Codeblocks.</h2>
-      <p>How I made the codeblocks</p>
+      <p>Used raw webpack importer</p>
+      <p>Added ESLinting rules when using create-react-app</p>
     </>
   );
 }
