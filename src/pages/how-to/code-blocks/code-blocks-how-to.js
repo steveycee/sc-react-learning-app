b27a0bf5a3822b93d9d20090c1f274a5e5f95942
steveycee <stevey.cee@gmail.com>
2022-02-26 19:08:16 +0000

    feat: added prism react renderer package

--- src/pages/how-to/code-blocks/code-blocks-how-to.js
+++ src/pages/how-to/code-blocks/code-blocks-how-to.js
@@ -4,8 +4,23 @@ function HowToCodeBlocks() {
   return (
     <>
       <h2>How To: Codeblocks.</h2>
+      <p>
+        Everything below is my write up of what I learned from this{" "}
+        <a
+          href="https://javascript.plainenglish.io/import-a-file-as-a-raw-string-in-gatsby-or-react-c788d7273bcb"
+          target="_blank"
+          rel="noreferrer"
+        >
+          blog
+        </a>
+        .
+      </p>
       <p>Used raw webpack importer</p>
       <p>Added ESLinting rules when using create-react-app</p>
+      <p>The code block component</p>
+      <p>Props in the code block component</p>
+      <p>Basic Styling</p>
+      <p>Advanced Styling</p>
     </>
   );
 }
